a27832988a2a45e189b8251f0b4b9eb38e05d6f2
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2020-05-22 13:07:11 +0900

    update: プロフィールの表示順を変更

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const request = require('request');
 const line = require('@line/bot-sdk');
 const PORT = process.env.PORT || 5000;
 const config = {
-  channelAccessToken: process.env.ACCESS_TOKEN || 'xxxx',
-  channelSecret: process.env.SECRET_KEY || 'xxxx'
+  channelAccessToken: process.env.ACCESS_TOKEN || 'xxx',
+  channelSecret: process.env.SECRET_KEY || 'xxx'
 };
 const client = new line.Client(config);
 
@@ -13,7 +13,11 @@ express()
   .post('/hook/', line.middleware(config), (req, res) => lineBot(req, res))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-// LINEBot
+/**
+ * LINEBotメイン
+ * @param {Object} req requestオブジェクト
+ * @param {Object} res responseオブジェクト
+ */
 function lineBot(req, res) {
   res.status(200).end();
   const events = req.body.events;
@@ -24,42 +28,54 @@ function lineBot(req, res) {
       console.log(`メッセージイベントではありません : ${ev.type}`);
       continue;
     }; 
-    idol(ev);
+    searchIdol(ev);
   };
 };
 
-// 検索して返信
-function idol(ev) {
+/**
+ * プロフィールを検索して返信
+ * @param {Object} ev イベントオブジェクト
+ */
+function searchIdol(ev) {
   // テキスト以外ならreturn
   if (ev.message.type !== 'text') {
-    return client.replyMessage(ev.replyToken, {
+    client.replyMessage(ev.replyToken, {
       type: 'text',
       text: 'テキストでお願いします…！'
     });
+    return;
   };
+
   const text = ev.message.text;
-  // "誕生日"が含まれているか
+
+  // 検索条件指定
   let mode = 0;
   if (text.match(/誕生日/)) {
     mode = text.match(/明日/) ? 3 : text.match(/昨日/) ? 1 : 2;
   };
-  // 検索
+
+  // 検索して結果を返信
   searchIdolProfile(mode, text)
     .then((json) => createSendMessage(json))
     .then((flexMesseage) => {
       console.log('ok!');
-      return client.replyMessage(ev.replyToken, flexMesseage);
+      client.replyMessage(ev.replyToken, flexMesseage);
     })
     .catch((errorFlexMesseage) => {
       console.log('NotFound');
-      return client.replyMessage(ev.replyToken, errorFlexMesseage);
+      client.replyMessage(ev.replyToken, errorFlexMesseage);
     });
 };
 
-// im@sparqlからプロフィールを取得
+/**
+ * imasparqlからプロフィールを取得
+ * @param  {Number} mode     検索モード(0:名前検索/1:昨日が誕生日/2:今日が誕生日/3:明日が誕生日)
+ * @param  {String} idolName アイドル名
+ * @return {Object}          オブジェクトに変換したJSON
+ */
 function searchIdolProfile (mode, idolName) {
   return new Promise((resolve, reject) => {
-    // 検索条件 (0:名前検索/1:昨日が誕生日/2:今日が誕生日/3:明日が誕生日)
+    // 検索条件を指定
     let searchCriteria = '';
     if (mode === 0){
       searchCriteria = `?data rdfs:label ?名前;rdf:type ?type.FILTER(?type IN (imas:Idol,imas:Staff)).OPTIONAL{?data imas:nameKana ?名前ルビ.}OPTIONAL{?data imas:alternateNameKana ?名前ルビ.}OPTIONAL{?data imas:givenNameKana ?名前ルビ.}FILTER(CONTAINS(?名前,"${idolName}")||CONTAINS(?名前ルビ,"${idolName}")).`;
@@ -71,6 +87,7 @@ function searchIdolProfile (mode, idolName) {
       const nowDate = `${mon}-${day}`;
       searchCriteria = `?data rdfs:label ?名前;schema:birthDate ?BD.FILTER(regex(str(?BD),"${nowDate}")).OPTIONAL{?data imas:nameKana ?名前ルビ.}OPTIONAL{?data imas:alternateNameKana ?名前ルビ.}OPTIONAL{?data imas:givenNameKana ?名前ルビ.}`;
     };
+
     // クエリを作成
     const query = `
     PREFIX schema: <http://schema.org/>
@@ -78,7 +95,7 @@ function searchIdolProfile (mode, idolName) {
     PREFIX imas: <https://sparql.crssnky.xyz/imasrdf/URIs/imas-schema.ttl#>
     PREFIX foaf: <http://xmlns.com/foaf/0.1/>
     PREFIX rdfs:  <http://www.w3.org/2000/01/rdf-schema#>
-    SELECT DISTINCT ?名前 ?名前ルビ ?ニックネーム ?ニックネームルビ ?テーマカラー ?所属 ?性別 ?年齢 ?身長 ?体重 ?B ?W ?H ?誕生日 ?星座 ?血液型 ?利き手 ?出身地 ?説明 (GROUP_CONCAT(distinct ?Favorite;separator=',') as ?好きなもの) (GROUP_CONCAT(distinct ?Hobby;separator=',') as ?趣味) ?CV 
+    SELECT DISTINCT ?名前 ?名前ルビ ?ニックネーム ?ニックネームルビ ?テーマカラー ?所属 ?性別 ?年齢 ?身長 ?体重 ?B ?W ?H ?誕生日 ?星座 ?血液型 ?利き手 ?出身地 (GROUP_CONCAT(distinct ?Favorite;separator=',') as ?好きなもの) (GROUP_CONCAT(distinct ?Hobby;separator=',') as ?趣味) ?説明 ?CV 
     WHERE {
       {${searchCriteria}}
       OPTIONAL { ?data imas:Color ?テーマカラー. }
@@ -102,6 +119,8 @@ function searchIdolProfile (mode, idolName) {
     }GROUP BY ?名前 ?名前ルビ ?ニックネーム ?ニックネームルビ ?テーマカラー ?所属 ?性別 ?年齢 ?身長 ?体重 ?B ?W ?H ?誕生日 ?星座 ?血液型 ?利き手 ?出身地 ?説明 ?CV LIMIT 5
     `;
     const url = `https://sparql.crssnky.xyz/spql/imas/query?output=json&query=${encodeURIComponent(query)}`;
+
+    // im@sparqlにアクセス
     request.get(url, (err, res, body) => {
       if (!err && res.statusCode === 200) {
         const json = JSON.parse(body)['results']['bindings'];
@@ -114,7 +133,11 @@ function searchIdolProfile (mode, idolName) {
   });
 };
 
-// FlexMesseageを作成
+/**
+ * FlexMesseageのオブジェクトを作成
+ * @param  {Object} data オブジェクトに変換したJSON
+ * @return {Object}      FlexMesseageのオブジェクト
+ */
 function createSendMessage(data) {
   return new Promise((resolve, reject) => {
     const contents = [];
@@ -124,27 +147,35 @@ function createSendMessage(data) {
       reject(errorMsg('みつかりませんでした', 'ごめんなさい…(´+ω+｀)'));
     };
 
-    data.forEach((i, index) => {
+    // プロフィール情報を整形
+    data.forEach((i) => {
       const profile = [];
       const keys = Object.keys(i);
       const name = i[keys[0]]['value'];
       const imageColor = (keys[2] === 'テーマカラー') ? `#${i[keys[2]]['value']}` : '#ff73cc';
 
-      // 性別を日本語化する
+      // 性別を日本語に変換
       if (i['性別']) {
-        i['性別']['value'] = i['性別']['value'] === 'female' ?  '女性' : '男性';
+        i['性別']['value'] = (i['性別']['value'] === 'female') ? '女性' : '男性';
       };
-      // 利き手を日本語化する
+
+      // 利き手を日本語に変換
       if (i['利き手']) {
-        i['利き手']['value'] = i['利き手']['value'] === 'right' ? '右利き' : (i['利き手']['value'] === 'left' ? '左利き' : '両利き');
+        i['利き手']['value'] = (
+            (i['利き手']['value'] === 'right') ? '右利き'
+          : (i['利き手']['value'] === 'left')  ? '左利き'
+          : '両利き'
+        );
       };
-      // 誕生日のフォーマットを日本語形式にする
+
+      // 誕生日のフォーマットを日本語形式に変換
       if (i['誕生日']) {
         const month =Number(i['誕生日']['value'].substr(2,2));
         const day =Number(i['誕生日']['value'].substr(5,2));
         i['誕生日']['value'] =`${month}月${day}日`;
       };
-      // それぞれのキーが存在するか確認して、単位を追加する
+
+      // 単位を追加
       const regex = /[a-zA-Z0-9!-/:-@¥[-`{-~]/mu;
       const chkKey =['年齢', '身長', '体重', '血液型'];
       const unit = ['歳', 'cm', 'kg', '型'];
@@ -154,7 +185,7 @@ function createSendMessage(data) {
         };
       };
 
-      // プロフィール情報のオブジェクト配列
+      // プロフィール内容
       for (let j = 2; j < keys.length; j++) {
         const profValue = i[keys[j]]['value'];
         if (keys[j] === 'テーマカラー') continue;
@@ -183,7 +214,7 @@ function createSendMessage(data) {
         profile.push(profileContents);
       };
 
-      // flexMesseageのJSON
+      // flexMesseageのオブジェクト
       const flexJsonData = {
         "type": "bubble",
         "body": {
@@ -209,7 +240,7 @@ function createSendMessage(data) {
             {
               "type": "box",
               "layout": "vertical",
-              "contents": profile // プロフィールのオブジェクト配列
+              "contents": profile // プロフィール内容
             }
           ],
               "margin": "lg",
@@ -227,30 +258,32 @@ function createSendMessage(data) {
                 "uri": `http://www.google.co.jp/search?hl=ja&source=hp&q=${encodeURIComponent(`アイドルマスター ${name}`)}`
               },
               "style": "primary",
-              "color": imageColor　// テーマカラー
+              "color": imageColor // テーマカラー
             }
           ]
         }
       };
       contents.push(flexJsonData);
-      
-      // 完了を確認
-      if (index === data.length - 1) {
-        const result = {
-          'type': 'flex',
-          'altText': `${index + 1}件みつかりました！`,
-          'contents': {
-            'type': 'carousel',
-            'contents': contents
-          }
-        };
-        resolve(result);
-      };
     });
+    // オブジェクトを返す
+    const result = {
+      'type': 'flex',
+      'altText': `${data.length}件みつかりました！`,
+      'contents': {
+        'type': 'carousel',
+        'contents': contents
+      }
+    };
+    resolve(result);
   });
 };
 
-// エラーメッセージ
+/**
+ * エラーメッセージ
+ * @param  {String} title タイトル
+ * @param  {String} msg   エラー内容
+ * @return {Object}       メッセージオブジェクト
+ */
 function errorMsg(title, msg) {
   return {
     'type': 'flex',
