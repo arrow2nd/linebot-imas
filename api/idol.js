2527acf192733c6cb421b4934fa4b6fa3e42b852
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2020-07-25 15:40:11 +0900

    update: 日付指定で誕生日検索できる機能を追加

--- api/idol.js
+++ api/idol.js
@@ -2,24 +2,22 @@
 const request = require('request');
 const moment = require('moment-timezone');
 
+
 /**
- * キーワードからプロフィールを検索
- * @param  {String} text 検索キーワード
+ * メッセージからプロフィールを検索
+ * @param  {String} text メッセージ
  * @return {Object}      flexMessage
  */
-async function getIdolProfile(text){
-    let mode = 0;
+async function getIdolProfile(text) {
     let profile;
     let flexMessage;
-    
-    // 誕生日検索をするか
-    if (text.match(/誕生日/)) {
-        mode = text.match(/明日/) ? 3 : text.match(/昨日/) ? 1 : 2;
-    };
+
+    // メッセージを解析
+    text = parseMessage(text);
 
     // プロフィールを検索
     try {
-        profile = await search(mode, text);
+        profile = await search(text);
     } catch(err) {
         return err;
     };
@@ -37,27 +35,55 @@ async function getIdolProfile(text){
     return flexMessage;
 };
 
+
 /**
- * 検索
- * @param  {Number} mode 0: 名前検索/1: 昨日が誕生日/2: 今日が誕生日/3: 明日が誕生日
- * @param  {String} word 検索ワード
- * @return {Object}      成功時: プロフィールデータ/失敗時: flexMessage
+ * メッセージを解析して検索文字列を作成
+ * @param  {String} text メッセージ
+ * @return {String}      検索文字列
  */
-function search(mode, word) {
+function parseMessage(text) {
+    const formatList = ['M月D日', 'M/D'];
+    let date = '';
+
+    // 誕生日検索かチェック
+    if (text.match(/誕生日/)) {
+        const mode = text.match(/明日/) ? 3 : text.match(/昨日/) ? 1 : 2;
+        const addDays = mode - 2;
+        return moment().add(addDays, 'days').tz('Asia/Tokyo').format('MM-DD');
+    };
+
+    // メッセージが日付かどうかチェック
+    for (let format of formatList) {
+        date = moment(text, format, true);
+        if (date.isValid()) {
+            return date.format('MM-DD');
+        };
+    };
+
+    // メッセージをそのまま返す
+    return text;
+};
+
+
+/**
+ * 検索する
+ * @param  {String} words 検索文字列
+ * @return {Object}       成功時: プロフィールデータ
+ *                        失敗時: flexMessage
+ */
+function search(words) {
     return new Promise((resolve, reject) => {
-        // 検索条件を指定
         let searchCriteria = '';
-        if (mode === 0) {
-            searchCriteria = `?data rdfs:label ?名前;rdf:type ?type.FILTER(?type IN (imas:Idol,imas:Staff)).OPTIONAL{?data imas:nameKana ?名前ルビ.}OPTIONAL{?data imas:alternateNameKana ?名前ルビ.}OPTIONAL{?data imas:givenNameKana ?名前ルビ.}FILTER(CONTAINS(?名前,"${word}")||CONTAINS(?名前ルビ,"${word}")).`;
+
+        // MM-DD形式なら誕生日検索、それ以外なら通常検索
+        if (words.match(/^\d{1,2}-\d{1,2}/)) {
+            searchCriteria = `?data rdfs:label ?名前;rdf:type ?type;schema:birthDate ?BD.FILTER(?type IN (imas:Idol,imas:Staff)).FILTER(regex(str(?BD),"${words}")).OPTIONAL{?data imas:nameKana ?名前ルビ.}OPTIONAL{?data imas:alternateNameKana ?名前ルビ.}OPTIONAL{?data imas:givenNameKana ?名前ルビ.}`;
         } else {
-            const addDays = mode - 2;
-            const searchDate = moment().add(addDays, 'days').tz('Asia/Tokyo').format('MM-DD');
-            searchCriteria = `?data rdfs:label ?名前;rdf:type ?type;schema:birthDate ?BD.FILTER(?type IN (imas:Idol,imas:Staff)).FILTER(regex(str(?BD),"${searchDate}")).OPTIONAL{?data imas:nameKana ?名前ルビ.}OPTIONAL{?data imas:alternateNameKana ?名前ルビ.}OPTIONAL{?data imas:givenNameKana ?名前ルビ.}`;
+            searchCriteria = `?data rdfs:label ?名前;rdf:type ?type.FILTER(?type IN (imas:Idol,imas:Staff)).OPTIONAL{?data imas:nameKana ?名前ルビ.}OPTIONAL{?data imas:alternateNameKana ?名前ルビ.}OPTIONAL{?data imas:givenNameKana ?名前ルビ.}FILTER(CONTAINS(?名前,"${words}")||CONTAINS(?名前ルビ,"${words}")).`;
         };
 
         // クエリ(ながい)
-        const query = `
-        PREFIX schema: <http://schema.org/>
+        const query = `PREFIX schema: <http://schema.org/>
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
         PREFIX imas: <https://sparql.crssnky.xyz/imasrdf/URIs/imas-schema.ttl#>
         PREFIX foaf: <http://xmlns.com/foaf/0.1/>
@@ -99,6 +125,77 @@ function search(mode, word) {
         });
     });
 };
+
+
+/**
+ * 読みやすい表現に変換
+ * @param  {Object} data プロフィールデータ
+ * @return {Object}      変換後のプロフィールデータ
+ */
+function conversion(data){
+    // 作品名を変換
+    const series = {
+        'CinderellaGirls': '346Pro (CinderellaGirls)',
+        '1st Vision': '765Pro (旧プロフィール)',
+        'DearlyStars': '876Pro (DearlyStars)',
+        '315ProIdols': '315Pro (SideM)',
+        'MillionStars': '765Pro (MillionLive!)',
+        '283Pro': '283Pro (ShinyColors)',
+        '765AS': '765Pro (IDOLM@STER)',
+        '961ProIdols': '961Pro (IDOLM@STER)',
+        '1054Pro': '1054Pro (IDOLM@STER)'
+    };
+    if (data.所属){
+        const title = series[data.所属.value];
+        data.所属.value = (title) ? title : data.所属.value;
+    };
+    
+    // 性別を日本語に変換
+    const gender = {
+        male: '男性',
+        female: '女性'
+    };
+    if (data.性別) {
+        const jp = gender[data.性別.value];
+        data.性別.value = (jp) ? jp : '不明';
+    };
+
+    // 利き手を日本語に変換
+    const handedness = {
+        right: '右利き',
+        left: '左利き',
+        both: '両利き'
+    };
+    if (data.利き手) {
+        const jp = handedness[data.利き手.value];
+        data.利き手.value = (jp) ? jp : '不明';
+    };
+
+    // 誕生日のフォーマットを日本語形式に変換
+    if (data.誕生日) {
+        const month = Number(data.誕生日.value.substr(2, 2));
+        const day = Number(data.誕生日.value.substr(5, 2));
+        data.誕生日.value = `${month}月${day}日`;
+    };
+
+    // 単位を追加
+    const regex = /[a-zA-Z0-9!-/:-@¥[-`{-~]/mu;
+    const chkKey = ['年齢', '身長', '体重', '血液型'];
+    const unit = ['歳', 'cm', 'kg', '型'];
+    for (let i = 0; i < chkKey.length; i++) {
+        if (data[chkKey[i]] && regex.test(data[chkKey[i]].value)) {
+            data[chkKey[i]].value += unit[i];
+        };
+    };
+
+    //カラーコードに#をつける
+    if (data.カラー){
+        data.カラー.value = `#${data.カラー.value}`;
+    };
+
+    return data;
+};
+
 
 /**
  * flexMessageを作成
@@ -218,77 +315,9 @@ function createMessage(profileData){
     });
 };
 
-/**
- * 読みやすい表現に変換
- * @param  {Object} data プロフィールデータ
- * @return {Object}      変換後のプロフィールデータ
- */
-function conversion(data){
-    // 作品名を変換
-    const series = {
-        'CinderellaGirls': '346Pro (CinderellaGirls)',
-        '1st Vision': '765Pro (旧プロフィール)',
-        'DearlyStars': '876Pro (DearlyStars)',
-        '315ProIdols': '315Pro (SideM)',
-        'MillionStars': '765Pro (MillionLive!)',
-        '283Pro': '283Pro (ShinyColors)',
-        '765AS': '765Pro (IDOLM@STER)',
-        '961ProIdols': '961Pro (IDOLM@STER)',
-        '1054Pro': '1054Pro (IDOLM@STER)'
-    };
-    if (data.所属){
-        const title = series[data.所属.value];
-        data.所属.value = (title) ? title : data.所属.value;
-    };
-    
-    // 性別を日本語に変換
-    const gender = {
-        male: '男性',
-        female: '女性'
-    };
-    if (data.性別) {
-        const jp = gender[data.性別.value];
-        data.性別.value = (jp) ? jp : '不明';
-    };
-
-    // 利き手を日本語に変換
-    const handedness = {
-        right: '右利き',
-        left: '左利き',
-        both: '両利き'
-    };
-    if (data.利き手) {
-        const jp = handedness[data.利き手.value];
-        data.利き手.value = (jp) ? jp : '不明';
-    };
-
-    // 誕生日のフォーマットを日本語形式に変換
-    if (data.誕生日) {
-        const month = Number(data.誕生日.value.substr(2, 2));
-        const day = Number(data.誕生日.value.substr(5, 2));
-        data.誕生日.value = `${month}月${day}日`;
-    };
-
-    // 単位を追加
-    const regex = /[a-zA-Z0-9!-/:-@¥[-`{-~]/mu;
-    const chkKey = ['年齢', '身長', '体重', '血液型'];
-    const unit = ['歳', 'cm', 'kg', '型'];
-    for (let i = 0; i < chkKey.length; i++) {
-        if (data[chkKey[i]] && regex.test(data[chkKey[i]].value)) {
-            data[chkKey[i]].value += unit[i];
-        };
-    };
-
-    //カラーコードに#をつける
-    if (data.カラー){
-        data.カラー.value = `#${data.カラー.value}`;
-    };
-
-    return data;
-};
 
 /**
- * エラーメッセージ
+ * エラーメッセージを作成
  * @param  {String} title タイトル
  * @param  {String} msg   エラー内容
  * @return {Object}       メッセージオブジェクト
@@ -321,6 +350,7 @@ function errorMsg(title, msg) {
         }
     };
 };
+
 
 module.exports = {
     getIdolProfile
