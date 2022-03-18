71a423bfe83d5e6689a3dc1411857362ae9da643
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-03-18 16:52:53 +0900

    chore: mocha から jest に移行

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+export default {
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+
+  // An array of directory names to be searched recursively up from the requiring module's location
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
+
+  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  testPathIgnorePatterns: ['/node_modules/', '/.yarn/', '/public/']
+}
