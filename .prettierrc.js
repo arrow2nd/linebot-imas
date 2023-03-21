c765190eafb5082c225bb8ea7380ad48d0009b3c
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-03-21 16:17:39 +0900

    fix: pnpm で yarn のプラグインが使えない

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [require("@trivago/prettier-plugin-sort-imports")],
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: "none",
+  bracketSpacing: true,
+  arrowParens: "always",
+  importOrder: ["<THIRD_PARTY_MODULES>", "^[./]"],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true
+}
